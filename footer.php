67dc6947e0ad76fcd48c274c231864279adfe2ad
sora1129 <64789798+sora1129@users.noreply.github.com>
2021-09-30 15:40:57 +0900

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,21 @@
+    <!-- footer -->
+    <footer>
+      <div class="container">
+        <div class="logo">
+          <img src="<?php echo get_template_directory_uri(); ?>/images/nikocyann.png" alt="苫米地式気功ヒーラー養成クラスロゴ">
+        </div>
+        <a href="https://www.iyashinote.org/j/privacy" target="blank">プライバシーポリシー</a>
+        <p class="footer_txt">治療院癒しの手 気功講座事務局<br>
+        〒156-0043 東京都世田谷区松原2-42-12<br>
+        TEL.03-6336-5820<br>
+        E-MAIL.info@iyashinote.org</p>
+        <p class="footer_txtsm">*現在電話でのお問い合わせは受け付けておりません。<br>
+        お問い合わせはメールでお願いいたします。
+        <p class="copyright">Copyright © 2020 治療院癒しの手 サロン<br class="tb-br">| 橋本明頼 All Rights Reserved.</p>
+        </p>
+      </div>
+    </footer>
+    <?php wp_footer(); ?>
+    <!-- /footer -->    
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-W8NXM89');</script>
+  <!-- End Google Tag Manager -->
+    <meta charset="utf-8">
+    <meta http-equiv="content-type" content="text/html" charset="UTF-8">
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- <title>苫米地式気功ヒーラー養成クラス｜初めてでもヒーリングは簡単です</title> -->
+    <title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title>
+    <meta name="description" content="科学の気功と言われている「苫米地式気功」の養成クラスです。職場の人間関係や家族の健康、精神面などにお悩みを解決する力が身につきます。自分自身はもちろん、ご家族へのヒーリングもできるようになりますよ。苫米地の弟子、橋本明頼が初心者でも簡単にできるヒーリングを教えます。">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" />
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/responsive.css" type="text/css" />
+   <script src="https://kit.fontawesome.com/a4c3a8065a.js" crossorigin="anonymous"></script>
+
+    <!-- SNS -->
+    <meta property="og:title" content="苫米地式気功ヒーラー養成クラス" />
+    <meta property="og:type" content="LP" />
+    <meta property="og:url" content="ページの URL" />
+    <meta property="og:image" content="サムネイル画像の URL" />
+    <meta property="og:site_name" content="橋本明頼の苫米地式気功ヒーラー養成クラス" />
+    <meta property="og:description" content="科学の気功と言われている「苫米地式気功」の養成クラスです。職場の人間関係や家族の健康、精神面などにお悩みを解決する力が身につきます。自分自身はもちろん、ご家族へのヒーリングもできるようになりますよ。苫米地の弟子、橋本明頼が初心者でも簡単にできるヒーリングを教えます。" />
+  </head>
+
+  <body>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W8NXM89"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+
+  <?php wp_head(); ?>
